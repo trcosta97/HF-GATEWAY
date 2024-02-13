e6b7d67a0cb06c40af7e575f54b2a07824b6d8df
Thiago Ribeiro <trcosta97@hotmail.com>
2024-02-13 17:40:11 -0300

    gateway

---
+++ src/main/java/com/hyperfoods/gateway/HfGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hyperfoods.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HfGatewayApplication.class, args);
+	}
+
+}
